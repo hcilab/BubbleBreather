a0bfe952116ff57617465a171cfca3cd4b94d437
Aaron W. Tabor <aarontabor50@gmail.com>
2020-07-13 15:54:36 -0300

    Created a `thumbnail` page that renders read-only view of saved paintings.
    The `thumbnail.html` page added in this commit renders a static / read-only version of an saved painting, specified by its painting id. For example, to render saved painting "XYZ", hit url: "./thumbnail.html?paintingid=XYZ"

---
+++ thumbnail.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // current painting is specified as a url parameter
+  // ...die if unspecified
+  let id = getURLParams().paintingid;
+  if (id == null || id == 0) {
+    console.log('Invalid or missing painting id...');
+    text('Invalid or missing painting id specified...', 0, 0, width);
+    return;
+  }
+
+  // Attempt to load corresponding painting from Window.localStorage
+  // ...die if invalid or unfound
+  let painting = Painting.load(id);
+  if (painting == null) {
+    console.log('Invalid painting id: ' + id);
+    text('Invalid painting id specified (' + id + ')...', 0, 0, width);
+    return;
+  }
+
+  // Otherwise draw painting
+  painting.bubbles.forEach(s => s.draw());
+
+  //TODO: Draw summary metrics...
+}
